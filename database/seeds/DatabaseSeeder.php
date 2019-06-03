4be8e132ce498a5f7d4622795f98cc2c327c4954
Aron Smit <aronsmit@hotmail.com>
2019-06-03 15:14:12 +0200

    Revert "setup database with fake data"
    This reverts commit 37c4f55fcb9de528a4e92ace0208f54ccf50c76a.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\Link::class, 5)->create();
         // $this->call(UsersTableSeeder::class);
     }
 }
